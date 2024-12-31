3334c887d83c380d60e771aa831f0355a4300a33
adalix <gumustolgahan@gmail.com>
2025-01-01 02:41:41 +0300

    added: videos data

---
+++ videos.js
@@ -0,0 +1,6 @@
+export const videos = [
+    {id: 1, videoSrc:"/media/wavey.mp4"},
+    {id: 2, videoSrc:"/media/cocacola-video.mp4"},
+    {id: 3, videoSrc:"/media/minced-meat.mp4"},
+    {id: 4, videoSrc:"/media/landing-LTBJ.mp4"}
+]
