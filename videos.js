42e0f633e8d48a7e53d09eed44c4c9044c8b664e
adalix <gumustolgahan@gmail.com>
2025-01-04 01:56:34 +0300

    changed data

--- videos.js
+++ videos.js
@@ -2,5 +2,6 @@ export const videos = [
     {id: 1, videoSrc:"/media/wavey.mp4"},
     {id: 2, videoSrc:"/media/cocacola-video.mp4"},
     {id: 3, videoSrc:"/media/minced-meat.mp4"},
-    {id: 4, videoSrc:"/media/landing-LTBJ.mp4"}
+    {id: 4, videoSrc:"/media/landing-LTBJ.mp4"},
+    {id: 5, videoSrc:"/media/gaming-footage(1).mp4"}
 ]
