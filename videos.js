c38994a9d5c05dbdacbd1b2f109ef7a8a7522285
adalix <gumustolgahan@gmail.com>
2025-01-05 02:05:23 +0300

    added screenshots , video names and durations

--- videos.js
+++ videos.js
@@ -1,7 +1,7 @@
 export const videos = [
-    {id: 1, videoSrc:"/media/wavey.mp4"},
-    {id: 2, videoSrc:"/media/cocacola-video.mp4"},
-    {id: 3, videoSrc:"/media/minced-meat.mp4"},
-    {id: 4, videoSrc:"/media/landing-LTBJ.mp4"},
-    {id: 5, videoSrc:"/media/gaming-footage(1).mp4"}
+    {id: 1, videoImg:'/media/screenshots/wavey.png', videoSrc:"/media/wavey.mp4", name: "wavey", duration: "00:06"},
+    {id: 2, videoImg:'/media/screenshots/cocacola.png', videoSrc:"/media/cocacola.mp4", name: "coca-cola",duration: "00:31"},
+    {id: 3, videoImg:'/media/screenshots/flowfield.png', videoSrc:"/media/flowfield.mp4", name: "flowfield",duration: "00:12"},
+    {id: 4, videoImg:'/media/screenshots/landing.png', videoSrc:"/media/landing.mp4", name: "landing",duration: "04:00"},
+    {id: 5, videoImg:'/media/screenshots/gaming-footage.png', videoSrc:"/media/gaming-footage.mp4", name: "gaming",duration: "00:57"}
 ]
