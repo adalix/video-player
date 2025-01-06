3477a87d6cdf6658d94a622af03f194ee740c75d
adalix <gumustolgahan@gmail.com>
2025-01-07 00:42:35 +0300

    changed some videos and images name

--- videos.js
+++ videos.js
@@ -1,7 +1,8 @@
 export const videos = [
-    {id: 1, videoImg:'/media/screenshots/wavey.png', videoSrc:"/media/wavey.mp4", name: "wavey", duration: "00:06"},
-    {id: 2, videoImg:'/media/screenshots/cocacola.png', videoSrc:"/media/cocacola.mp4", name: "coca-cola",duration: "00:31"},
-    {id: 3, videoImg:'/media/screenshots/flowfield.png', videoSrc:"/media/flowfield.mp4", name: "flowfield",duration: "00:12"},
-    {id: 4, videoImg:'/media/screenshots/landing.png', videoSrc:"/media/landing.mp4", name: "landing",duration: "04:00"},
-    {id: 5, videoImg:'/media/screenshots/gaming-footage.png', videoSrc:"/media/gaming-footage.mp4", name: "gaming",duration: "00:57"}
+    {id: 1, videoImg:'/media/screenshots/wavey.png', videoSrc:"/media/wavey.mp4", name: "Wavey", duration: "00:06"},
+    {id: 2, videoImg:'/media/screenshots/marlboro.png', videoSrc:"/media/marlboro.mp4", name: "Marlboro",duration: "01:26"},
+    {id: 3, videoImg:'/media/screenshots/flowfield.png', videoSrc:"/media/flowfield.mp4", name: "Flowfield",duration: "00:12"},
+    {id: 4, videoImg:'/media/screenshots/landing.png', videoSrc:"/media/landing.mp4", name: "Landing to LTBJ",duration: "04:00"},
+    {id: 5, videoImg:'/media/screenshots/gaming-footage.png', videoSrc:"/media/gaming-footage.mp4", name: "Gaming",duration: "00:57"},
+    {id: 6, videoImg:'/media/screenshots/constellations-effect.png', videoSrc:"/media/constellations-effect.mp4", name: "Constellations effect",duration: "01:07"}
 ]
